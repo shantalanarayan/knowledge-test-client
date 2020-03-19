ce9d8addb69826a4dbd26d6f12809af77b6be6b8
shantalanarayan <mail2shantala@gmail.com>
2020-03-18 23:59:33 -0400

    Added background image for the body and updated some elements Added background image for the body Moved sign-out button and user logged in message to navbar Moved feedback message to bottom of the screen

--- assets/scripts/shared/ui.js
+++ assets/scripts/shared/ui.js
@@ -6,10 +6,12 @@ const toggleForm = () => {
   if (store.user) {
     // User is logged in
     $('#auth-area').show()
+    $('#sign-out').show()
     $('#non-auth-area').hide()
   } else {
     // User is logged out
     $('#auth-area').hide()
+    $('#sign-out').hide()
     $('#non-auth-area').show()
   }
 }
@@ -30,7 +32,7 @@ const displayMessage = (message, isSuccess) => {
 const setWelcomeMessage = () => {
   if (store.user) {
     // User is logged in. Hence, set welcome message
-    $('#user-welcome').text('Welcome ' + store.user.email)
+    $('#user-welcome').text('Hello ' + store.user.email + '!')
   } else {
     // User is logged out. Hence, clear welcome message
     $('#user-welcome').empty()
